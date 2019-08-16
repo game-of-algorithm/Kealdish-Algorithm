3c0be0054365e9ff2c85e613908f2452f6248025
kealdish <1248800415@qq.com>
2019-08-16 22:44:16 +0800

    Add max heap sort function

--- HeapSortFunc.cc
+++ HeapSortFunc.cc
@@ -1,4 +1,67 @@
 /**
+ * 大根堆排序
+ */
+
+#include<iostream>
+using namespace std; 
+void heapSort(int a[], int n);
+void maxHeap(int a[], int n);
+void buildMaxHeap(int a[], int n);
+ 
+int size;
+int main()
+{
+	int a[] = { 9, 12, -3, 0, 6, 8, 15, 7 };
+	size= sizeof(a) / 4;
+	heapSort(a, size);
+	for (int i = 0; i < 8; i++)
+		cout << a[i] << " ";
+	return 0;
+}
+
+void heapSort(int a[], int n)
+{
+	int i;
+	buildMaxHeap(a, n);
+	for (i = size; i>1; i--)
+	{
+		swap(a[0], a[i-1]);
+	    size = size - 1;
+		maxHeap(a, 1);
+	}
+ 
+}
+
+void buildMaxHeap(int a[], int n)
+{
+	int i = n / 2;
+	for (i; i > 0; i--)
+		maxHeap(a, i);
+ 
+}
+
+void maxHeap(int a[], int n)
+{
+	int leftChild, rightChild, largest;
+	leftChild = 2 * n;
+	rightChild = 2 * n + 1;
+
+	int q = sizeof(a);
+	if (leftChild <= size && a[leftChild-1] > a[n-1])
+		largest = leftChild;
+	else
+		largest = n;
+	if (rightChild <= size&&a[rightChild-1] > a[largest-1])
+		largest = rightChild;
+
+	if (largest != n)
+	{
+		swap(a[n-1], a[largest-1]);
+		maxHeap(a, largest);
+	}
+}
+
+/**
  * 2.4.16 对于 N = 32，构造数组使得堆排序使用的比较次数最多以及最少。
  */
 
@@ -7,8 +70,17 @@
  * 
  * 比较次数为 90 次
  * 
+ * 反向来看，对于一个已经构建好的大堆，在缩小堆排序数组时要想比较次数最多就需要交换次数最多，
+ * 要交换次数最多，就需在堆顶元素与堆尾元素交换后下沉最多，要想下沉最多可以在堆顶元素与堆尾元素交换时让堆尾元素是堆中的最小元素。
+ * 按照这个方法就可以构建一个比较次数最多的大堆。反过来操作就可以得到一个比较次数最少的大堆。
  * 
  */
+
+public class WorstHeap {
+    
+};
+
+
 
 
 /**
