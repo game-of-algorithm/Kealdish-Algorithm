fea063421e5c2194844d7e36d6fd03f7c9ed55c1
kealdish <1248800415@qq.com>
2019-08-28 22:52:32 +0800

    Create frequency_counter.cc

---
+++ 3.1/frequency_counter.cc
@@ -0,0 +1,34 @@
+/**
+ * 修改 frequencyCounter ，打印出现频率最高的所有单词，而非其中之一。提示：请用 queue。
+ */
+
+#include <vector>
+#include <string>
+using std::vector;
+using std::string;
+
+void search() {
+    string max = "";
+    vector<string> queue;
+
+    // st 是符号表，懒得实现了
+    st.put(max, 0);
+    vector<string> keys = st.keys();
+
+    for (int i = 0; i < keys.size(); i++)
+    {
+        string key = keys[i];
+
+        if (st.get(key) > st.get(max))
+        {
+            max = key;
+            vt.clear();
+            vt.push_back(key);
+        } else if (st.get(key) == st.get(max))
+        {
+            vt.push_back(key);
+        }
+        
+    }
+    
+}
